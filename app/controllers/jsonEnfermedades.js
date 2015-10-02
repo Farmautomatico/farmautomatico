c70a8ea95346e03f75aa12132fa196546a76394d
erickfmm <erickfmm@gmail.com>
2015-10-01 23:31:15 -0300

    último commit al master por ahora...
    eliminación de archivos demás, muchos archivos demás y arreglé ciertas
    cosillas.
    Ahora está el typeahead de especialista también.
    Por favor commiteen en alguna parte para saber qué se puede o no
    modificar.
    Ahora commitearé a mi rama el combo dependiente (me aseguraré de no
    interferir en lo ya hecho) y el ingresar y logout.
    Y por favor, vean que sea compatible, en especial porque modifiqué
    algunos POST y los pasé a GET, y las variables pasadas cambiaron de
    nombre...

--- app/controllers/jsonEnfermedades.js
+++ app/controllers/jsonEnfermedades.js
@@ -12,6 +12,18 @@ module.exports = function (app) {
     app.use('/', router);
 
       router.get('/jsonEnfermedades', function (req, res, next) {
+
+        db.enfermedad.encontrarEnfermedades().then(function(nombreEnfermedad) {
+          console.log("enfermedades");
+          console.log(nombreEnfermedad);
+          enfermedades=[];
+          for(i in nombreEnfermedad){
+            enfermedades.push(nombreEnfermedad[i].nombre_enfermedad);
+          }
+          console.log(enfermedades);
+          res.json(JSON.stringify(enfermedades));
+
+        })
         /*db.remedios.encontrarNombresRemedios().then(function(nombreRemedios) {
           remedios = [];
           for(i in nombreRemedios) {
