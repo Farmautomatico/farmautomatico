615e08a1e5974445f76d082864c0763a0a7b5d3d
Farmautomatico <erick.merinom@alumnos.uv.cl>
2015-10-15 19:43:19 -0300

    Caso Especialista

--- app/models/especialista.js
+++ app/models/especialista.js
@@ -1,4 +1,49 @@
+
 module.exports = function (sequelize, DataTypes) {
+
+  var especialista = sequelize.define('especialista', {
+	
+	id_especialista: DataTypes.INTEGER,
+	nombre_especialista: DataTypes.STRING,
+	fotoData: Types.STRING,
+	email: DataTypes.STRING,
+	especialidad: DataTypes.STRING,
+	direccion: DataTypes.STRING,
+	comunas_idcomunas: DataTypes.INTEGER
+	telefono: DataTypes.INTEGER
+	
+  }, {
+  
+    classMethods:{
+			encontrarEspecialistaTodos: function(ciudadpasada, enfermedad, edad){ return sequelize
+				  .query('select * from especialistas as e join telefonoespecialista as t on e.id_especialista = t.especialistas_id_especialista where e.comunas_idcomunas= ? and e.id_especialista IN (select especialistas_id_especialista from enfermedades_tiene_especialistas where enfermedad_nombre_enfermedad= ?)',
+				  { raw: true, replacements: [ciudadpasada, enfermedad], type: sequelize.QueryTypes.SELECT })
+			},
+
+			encontrarEspecialistaOdontologo: function(ciudadpasada, enfermedad, edad){ return sequelize
+				  .query('select * from especialistas as e join telefonoespecialista as t on e.id_especialista = t.especialistas_id_especialista where e.comunas_idcomunas= ? and e.id_especialista IN (select especialistas_id_especialista from enfermedades_tiene_especialistas where enfermedad_nombre_enfermedad= ?) and especialidad = odontologo',
+				  { raw: true, replacements: [ciudadpasada, enfermedad], type: sequelize.QueryTypes.SELECT })
+			},
+
+			encontrarEspecialistaKinesiologo: function(ciudadpasada, enfermedad, edad){ return sequelize
+				  .query('select * from especialistas as e join telefonoespecialista as t on e.id_especialista = t.especialistas_id_especialista where e.comunas_idcomunas= ? and e.id_especialista IN (select especialistas_id_especialista from enfermedades_tiene_especialistas where enfermedad_nombre_enfermedad= ?) and especialidad = kinesiologo',
+				  { raw: true, replacements: [ciudadpasada, enfermedad], type: sequelize.QueryTypes.SELECT })
+			},
+
+			encontrarEspecialistaCirujano: function(ciudadpasada, enfermedad, edad){ return sequelize
+				  .query('select * from especialistas as e join telefonoespecialista as t on e.id_especialista = t.especialistas_id_especialista where e.comunas_idcomunas= ? and e.id_especialista IN (select especialistas_id_especialista from enfermedades_tiene_especialistas where enfermedad_nombre_enfermedad= ?) and especialidad = cirujano',
+				  { raw: true, replacements: [ciudadpasada, enfermedad], type: sequelize.QueryTypes.SELECT })
+			}
+    }
+	
+  });
+	return especialista
+
+};
+
+
+
+/*module.exports = function (sequelize, DataTypes) {
 
   var especialista = sequelize.define('farmacia', {
   }, {
@@ -20,3 +65,4 @@ module.exports = function (sequelize, DataTypes) {
 	return especialista
 
 };
+*/
