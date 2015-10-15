2444e8d96eb7901729c6e59699c4b1537280ad6d
Farmautomatico <erick.merinom@alumnos.uv.cl>
2015-10-15 20:01:09 -0300

    Update especialista.js

--- app/controllers/especialista.js
+++ app/controllers/especialista.js
@@ -1,4 +1,95 @@
+var express = require('express'),
+  router = express.Router(),
+  db = require('../models');
 
+module.exports = function (app) {
+
+    // var session = require('express-session');
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+    /*	app.use(session({
+	  secret: 'farmautomatico',
+	  resave: false,
+	  saveUninitialized: true
+	}));*/
+	app.use('/', router);
+
+
+	
+	router.get('/especialista', function (req, res, next) {
+		
+		res.render('especialista', {
+			title: "Farmautomático",
+			especialistas: [],
+			ciudad: req.query.ciudad,
+			especialidad: req.query.especialidad,
+			enfermadad: req.query.enfermedad,
+			edad: req.query.enfermadad
+         });
+	});
+	router.get('/especialistaformulario', function (req, res, next) {
+		ciudad: req.query.ciudad,
+		enfermadad: req.query.enfermedad,
+		edad: req.query.enfermadad
+
+		switch (req.query.especialidad) {
+            case 'odontologo':
+                  db.especialista.encontrarEspecialistaOdontologo(ciudad, enfermedad, edad).then(callbackcmform);
+                  break;
+			case 'kinesiologo'
+                  db.especialista.encontrarEspecialistaKinesiologo(ciudad, enfermedad, edad).then(callbackcmform);
+                  break;
+			case 'cirujano'
+                  db.especialista.encontrarEspecialistaCirujano(ciudad, enfermedad, edad).then(callbackcmform);
+                  break;
+            default:
+                  db.especialista.encontrarEspecialistaTodos(ciudad, enfermedad, edad).then(ciudad, enfermedad, edad);
+                  break;
+        }
+
+	function callbackcmform(resul) {
+          var data = [], telef = [];
+		  console.log(resul);
+          for (var i = 0; i < resul.length; i++) {
+              var dataint = [];
+              telef.push(resul[i].telefono);
+              if ((i < resul.length - 1) && (resul[i].id_especialista != resul[i + 1].id_especialista)) {
+                  data.push([
+					resul[i].nombre_especialista,
+					resul[i].especialidad,
+					resul[i].email,
+					resul[i].direccion,
+					telef
+				  ]);
+                  telef = [];
+              }
+              if (i == resul.length - 1) {
+                  data.push([
+					resul[i].nombre_especialista,
+					resul[i].especialidad,
+					resul[i].email,
+					resul[i].direccion,
+					telef
+                  ])
+              }
+          }
+          console.log(data);
+          res.render('especialistas', {
+              title: "Farmautomático",
+              especialistas: data,
+              ciudad: req.query.ciudad,
+        	  especialidad: req.query.especialidad,
+			  enfermadad: req.query.enfermedad,
+			  edad: req.query.enfermadad
+          });
+      }
+    })
+
+};
+/*
 var express = require('express'),
   router = express.Router(),
   db = require('../models');
@@ -17,7 +108,7 @@ router.get('/especialista', function (req, res, next) {
       (recordar qué dijo el profe)
       para poder pasar la ciudad al req.query del get de abajo
       (ver centrosmedicos)
-      */
+      *//*
 		    res.render('especialista', {
 
 		     title: "Farmautomático",
@@ -34,3 +125,4 @@ router.get('/especialistaformulario', function (req, res, next) {
 		 });
 
 });
+*/
